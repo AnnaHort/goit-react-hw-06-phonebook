95ad76288325c10030af230e64b86c80d9748811
AnnaHort <annahort3d@gmail.com>
2023-09-15 19:28:29 +0300

    add Redux Persist library delete read.me

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,36 @@
-import { createStore,combineReducers } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { devToolsEnhancer } from "@redux-devtools/extension";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
 import { contactsReducer } from './reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+}
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const enhancer = devToolsEnhancer();
 
-export const store = createStore(rootReducer,enhancer);
+export const store = createStore(persistedReducer, enhancer);
+export const persistor = persistStore(store);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
